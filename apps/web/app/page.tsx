8c225b6fa921fffc6f740d7e94ec52859d33f39d
Anas Almutary <me@ianas.me>
2025-04-17 13:19:46 +0300

    chore(web): update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,33 +1,10 @@
-import Image, { type ImageProps } from "next/image";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-        <ThemeImage
-          srcLight="/images/next.svg"
-          srcDark="/images/next-dark.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-        />
         
       </div>
     </main>
---
+++ apps/web/app/provider.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "next-themes";
+
+export default function AppProvider({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+            {children}
+        </ThemeProvider>
+    );
+}
