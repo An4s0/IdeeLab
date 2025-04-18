6834cc9ca0af45f6d2f4f03f9a1c3f49e448259f
Anas Almutary <me@ianas.me>
2025-04-18 20:16:39 +0300

    chore(web): add sun & moon icons

--- apps/web/components/icons/index.ts
+++ apps/web/components/icons/index.ts
@@ -1,4 +1,5 @@
 import Error404 from "./error404";
 import ArrowRight from "./arrow-right";
+import { Sun, Moon } from "./theme";
 
-export { Error404, ArrowRight };
+export { Error404, ArrowRight, Sun, Moon };
