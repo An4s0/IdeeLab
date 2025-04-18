8752060743a992158c2e0d29e72f0dff49733144
Anas Almutary <me@ianas.me>
2025-04-19 01:11:53 +0300

    chore(web): add features section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 import Link from "next/link";
-import { ArrowRight } from "@/components/icons";
+import {
+  ArrowRightIcon,
+  ChartLineIcon,
+  PieChartLineIcon,
+  SparklesIcon,
+  UserCheckIcon,
+  UsersIcon,
+  UserGroupIcon,
+  DiamondTrophyIcon,
+  UserCogIcon,
+} from "@/components/icons";
 
 export default function Home() {
   return (
-    <main className="relative overflow-hidden min-h-screen">
+    <main className="relative overflow-hidden min-h-screen transition">
       {/* Hero Section */}
       <section className="flex flex-col items-center justify-center text-center space-y-2 md:space-y-4 mt-32 py-20 overflow-hidden max-w-2xl md:max-w-3xl mx-auto px-2 md:px-1">
         <p className="text-subtle text-md md:text-xl sm:text-lg">
@@ -25,7 +35,7 @@ export default function Home() {
               <span className="flex items-center gap-1">
                 Start Challenge
                 <div className="ml-1 transition-transform group-hover:translate-x-1">
-                  <ArrowRight size={16} />
+                  <ArrowRightIcon size={16} />
                 </div>
               </span>
               <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
@@ -57,6 +67,127 @@ export default function Home() {
           </div>
           <div>
             <span className="font-bold text-foreground">100%</span> free
+          </div>
+        </div>
+      </section>
+
+      {/* Features Section */}
+      <section className="flex flex-col items-center justify-center space-y-2 mt-20 max-w-6xl mx-auto px-2 md:px-1">
+        <h2 className="text-3xl font-semibold sm:text-4xl">
+          Why choose <span className="text-primary">IdeeLab</span>?
+        </h2>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 w-full mt-8 px-4">
+          <div className="group border border-outline/50 bg-feature-bg rounded-xl col-span-2 h-88 px-6 py-8 cursor-pointer hover:border-outline flex justify-between">
+            <div className="w-full md:w-1/2 flex flex-col justify-between">
+              <div>
+                <h2 className="font-semibold mb-2 flex justify-start items-center">
+                  <ChartLineIcon
+                    size={20}
+                    className="inline-block mr-2 text-foreground"
+                  />
+                  Real-Time Progress Tracking
+                </h2>
+                <p className="text-sm text-subtle">
+                  Track your progress with real-time analytics. See how you
+                  improve over time and stay motivated.
+                </p>
+              </div>
+              <ul className="text-sm text-subtle space-y-1 pl-1">
+                <li className="flex items-center">
+                  <span className="mr-2 text-primary">✔</span> Live stats on
+                  every challenge
+                </li>
+                <li className="flex items-center">
+                  <span className="mr-2 text-primary">✔</span> Visual progress
+                  over time
+                </li>
+                <li className="flex items-center">
+                  <span className="mr-2 text-primary">✔</span> Consistency
+                  streak tracking
+                </li>
+              </ul>
+            </div>
+            <PieChartLineIcon
+              size={220}
+              className="text-outline/50 group-hover:text-subtle/70 m-8 transition-colors duration-300 hidden md:block"
+            />
+          </div>
+          <div className="group border border-outline/50 bg-feature-bg rounded-xl col-span-2 lg:col-span-1  h-88 px-6 py-8 cursor-pointer hover:border-outline flex flex-col justify-between">
+            <div className="w-full">
+              <h2 className="font-semibold mb-2 flex justify-start items-center">
+                <SparklesIcon
+                  size={20}
+                  className="inline-block mr-2 text-foreground"
+                />
+                Daily Curated Challenges
+              </h2>
+              <p className="text-sm text-subtle">
+                Fresh and exciting challenges every day to sharpen your skills
+                and keep you engaged.
+              </p>
+            </div>
+            <div className="flex justify-center items-center">
+              <DiamondTrophyIcon
+                size={180}
+                className="text-outline/50 group-hover:text-subtle/70 transition-colors duration-300"
+              />
+            </div>
+          </div>
+          <div className="group border border-outline/50 bg-feature-bg rounded-xl col-span-2 lg:col-span-1 h-88 px-6 py-8 cursor-pointer hover:border-outline flex flex-col justify-between">
+            <div className="w-full">
+              <h2 className="font-semibold mb-2 flex justify-start items-center">
+                <UserCheckIcon
+                  size={20}
+                  className="inline-block mr-2 text-foreground"
+                />
+                Personalized Experience
+              </h2>
+              <p className="text-sm text-subtle">
+                Challenges, recommendations, and feedback tailored to your skill
+                level and goals.
+              </p>
+            </div>
+            <div className="flex justify-center items-center">
+              <UserCogIcon
+                size={180}
+                className="text-outline/50 group-hover:text-subtle/70 transition-colors duration-300"
+              />
+            </div>
+          </div>
+          <div className="group border border-outline/50 bg-feature-bg rounded-xl col-span-2 h-88 px-6 py-8 cursor-pointer hover:border-outline flex justify-between">
+            <div className="w-full md:w-1/2 flex flex-col justify-between">
+              <div>
+                <h2 className="font-semibold mb-2 flex justify-start items-center">
+                  <UsersIcon
+                    size={20}
+                    className="inline-block mr-2 text-foreground"
+                  />
+                  Community & Collaboration
+                </h2>
+                <p className="text-sm text-subtle">
+                  Learn and grow with a passionate community of coders. Share
+                  solutions, give feedback, and grow together.
+                </p>
+              </div>
+              <ul className="text-sm text-subtle space-y-1 pl-1">
+                <li className="flex items-center">
+                  <span className="mr-2 text-primary">✔</span> Public solutions
+                  & discussions
+                </li>
+                <li className="flex items-center">
+                  <span className="mr-2 text-primary">✔</span> Challenge
+                  leaderboards
+                </li>
+                <li className="flex items-center">
+                  <span className="mr-2 text-primary">✔</span> Peer reviews &
+                  feedback
+                </li>
+              </ul>
+            </div>
+            <UserGroupIcon
+              size={220}
+              className="text-outline/50 group-hover:text-subtle/70 m-8 transition-colors duration-300 hidden md:block"
+            />
           </div>
         </div>
       </section>
