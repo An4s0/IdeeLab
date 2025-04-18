2586a3bdf401bad6df229c3051a5c7af3df78536
Anas Almutary <me@ianas.me>
2025-04-18 20:17:25 +0300

    chore(web): update header

--- apps/web/components/header/header.tsx
+++ apps/web/components/header/header.tsx
@@ -2,7 +2,8 @@
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import Link from "next/link";
+import { Sun, Moon } from "../icons";
 
 export default function Header() {
   const { theme, setTheme } = useTheme();
@@ -16,16 +17,32 @@ export default function Header() {
     return null;
   }
   return (
-    <motion.header
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6 }}
-      className={"flex justify-between items-center p-4"}
-    >
-      <Image src="/logo.svg" alt="Logo" width={34} height={34} />
-      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-        Toggle Theme
-      </button>
-    </motion.header>
+    <header className="fixed w-full top-0 left-0 right-0 z-50 border-b border-outline bg-background/80 backdrop-blur">
+      <div className="flex justify-between items-center p-2 max-w-7xl mx-auto">
+        <div className="flex items-center space-x-4">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo" width={34} height={34} />
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <div
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            className="p-2 rounded-md hover:bg-outline/20 cursor-pointer"
+          >
+            {theme === "dark" ? (
+              <Sun size={20} className="text-yellow-500" />
+            ) : (
+              <Moon size={20} className="text-gray-800" />
+            )}
+          </div>
+          <Link
+            href="/sign-in"
+            className="inline-flex items-center py-1 px-5 rounded-md border border-primary/50 bg-primary/40 text-sm hover:bg-primary/60 hover:border-primary"
+          >
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 }
