200f178e5b4c386ca8fe745e50686d9de1f37982
Anas Almutary <00063q@gmail.com>
2025-05-14 17:55:42 +0300

    add github lib

---
+++ apps/web/lib/github.ts
@@ -0,0 +1,12 @@
+const github = {
+  redirect: async () => {
+    try {
+      const githubAuthURL = `${process.env.NEXT_PUBLIC_API_URL}/auth/github`;
+      window.location.href = githubAuthURL;
+    } catch (error) {
+      console.error("Error redirecting to GitHub:", error);
+    }
+  },
+};
+
+export default github;
