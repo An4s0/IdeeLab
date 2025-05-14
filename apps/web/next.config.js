7b9212d5bfea1aa9f0eec60e332b0e1a843845d0
Anas Almutary <00063q@gmail.com>
2025-05-14 17:57:56 +0300

    update deps and add tsup for build api

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 import dotenv from 'dotenv';
-dotenv.config({path: '../../.env'});
-const nextConfig = {};
+dotenv.config({ path: '../../.env' });
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname: '/u/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/a/**',
+            },
+        ]
+    },
+};
 
 export default nextConfig;
