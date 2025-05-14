d228250b2b44c4aedfc46b158c1bc8cc8c00f831
Anas Almutary <00063q@gmail.com>
2025-05-14 17:52:57 +0300

    add logout page

--- apps/web/app/(auth)/logout/logout.tsx
+++ apps/web/app/(auth)/logout/logout.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 import { useEffect } from "react";
 import cookies from "@/lib/cookies";
 
 export default function Logout() {
-    useEffect(() => {
-        cookies.remove("token");
-    
-        window.location.href = "/";
-    }, []);
+  useEffect(() => {
+    cookies.remove("token");
+
+    window.location.href = "/";
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold">Logging out...</h1>
