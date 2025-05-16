24dfd310461a104a13c8055a2cc8cf8ce3288c80
Anas Almutary <00063q@gmail.com>
2025-05-16 12:39:36 +0300

    Update middleware logic to handle user verification

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,40 +1,22 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 import user from "@/lib/user";
 
 export async function middleware(request: NextRequest) {
-  const url = request.nextUrl;
-  const token = request.cookies.get("token")?.value;
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value || "";
 
-  const isAuthPage = ["/login", "/register"].some((path) =>
-    url.pathname.startsWith(path),
-  );
+  const userData = await user.get(token);
 
-  if (!token) {
-    if (isAuthPage) {
-      return NextResponse.next();
-    }
-
-    return NextResponse.redirect(new URL("/login", url));
+  if (!userData.success) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  const res = await user.verifyToken(token);
-
-  if (!res.success) {
-    return NextResponse.redirect(new URL("/login", url));
+  if (!userData.data?.isVerified) {
+    return NextResponse.redirect(new URL("/verify-email", request.url));
   }
-
-  const resVerify = await user.get();
-  if (!resVerify.data?.isVerified) {
-    return NextResponse.redirect(new URL("/verify-email", url));
-  }
-
-  if (isAuthPage) {
-    return NextResponse.redirect(new URL("/", url));
-  }
-
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/challenges", "/login", "/register"],
+  matcher: ["/challenges"],
 };
