4d3bb492b152e7fc974f0c7e9bfc180028704089
Anas Almutary <00063q@gmail.com>
2025-05-23 01:39:33 +0300

    update terms page

--- apps/web/app/terms/page.tsx
+++ apps/web/app/terms/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Terms of Service",
   description:
-    "These are the terms of service for our application. By using our application, you agree to these terms.",
+    "Review the terms and conditions for using IdeeLab to explore, share, and manage your programming ideas.",
 };
 
 import TermsPage from "./terms";
