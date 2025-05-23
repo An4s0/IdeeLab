436bb40362415762179215db7aa5a331385c5b1d
Anas Almutary <00063q@gmail.com>
2025-05-23 21:08:11 +0300

    Update login logic

--- apps/web/lib/user/login.ts
+++ apps/web/lib/user/login.ts
@@ -3,43 +3,45 @@ import { ApiResponse, User } from "@/types";
 import cookies from "../cookies";
 
 interface LoginDataResponse {
-    user: User;
-    token: string;
+  user: User;
+  token: string;
 }
 
 export default async function login(credentials: {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }): Promise<ApiResponse<LoginDataResponse>> {
-    try {
-        const res = await axios({
-            url: process.env.NEXT_PUBLIC_API_URL + "/user/login",
-            method: "post",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            data: {
-                ...credentials,
-            },
-        });
+  try {
+    const res = await axios({
+      url: process.env.NEXT_PUBLIC_API_URL + "/user/login",
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        ...credentials,
+      },
+    });
 
-        if (res.data.success) {
-            // Set the token in cookies
-            cookies.set("token", res.data.data.token, 7);
-        }
-
-        return res.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            return {
-                success: false,
-                message: error.response?.data?.message || "Unknown error occurred during login",
-            };
-        } else {
-            return {
-                success: false,
-                message: "Unexpected error during login",
-            };
-        }
+    if (res.data.success) {
+      // Set the token in cookies
+      cookies.set("token", res.data.data.token, 7);
     }
+
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        success: false,
+        message:
+          error.response?.data?.message ||
+          "Unknown error occurred during login",
+      };
+    } else {
+      return {
+        success: false,
+        message: "Unexpected error during login",
+      };
+    }
+  }
 }
