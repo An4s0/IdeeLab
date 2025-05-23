c41c1b7f30a6bc2c897e30b9b9dc5c8b739506ec
Anas Almutary <00063q@gmail.com>
2025-05-24 01:16:02 +0300

    add: apps/api/types/idea.ts

---
+++ apps/api/types/idea.ts
@@ -0,0 +1,25 @@
+export interface IdeaType {
+  id?: string;
+
+  title: string;
+  slug?: string;
+  summary: string;
+  description: string;
+  category: string;
+  tags: string[];
+
+  upvotes?: number;
+  downvotes?: number;
+  comments?: number;
+  views?: number;
+  status?: "draft" | "published" | "archived";
+  is_hot?: boolean;
+  difficulty: "Beginner" | "Intermediate" | "Advanced" | "Expert" | null;
+  edited?: boolean;
+
+  user_id: string;
+
+  created_at?: string;
+  updated_at?: string;
+  published_at?: string | null;
+}
