253d24e8d528a0a48f74aca230f1c29a642de5c2
Anas Almutary <00063q@gmail.com>
2025-05-23 21:08:11 +0300

    Update get current user logic

--- apps/web/lib/user/me.ts
+++ apps/web/lib/user/me.ts
@@ -3,34 +3,38 @@ import { ApiResponse, User } from "@/types";
 import cookies from "@/lib/cookies";
 
 interface MeDataResponse {
-    user: User;
+  user: User;
 }
 
-export default async function me(tokenC?: string): Promise<ApiResponse<MeDataResponse>> {
-    try {
-        // Get the token from cookies
-        const token = cookies.get("token");
+export default async function me(
+  tokenC?: string,
+): Promise<ApiResponse<MeDataResponse>> {
+  try {
+    // Get the token from cookies
+    const token = cookies.get("token");
 
-        const res = await axios({
-            url: process.env.NEXT_PUBLIC_API_URL + `/user/me`,
-            method: "get",
-            headers: {
-                Authorization: `Bearer ${tokenC || token}`,
-            },
-        });
+    const res = await axios({
+      url: process.env.NEXT_PUBLIC_API_URL + `/user/me`,
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${tokenC || token}`,
+      },
+    });
 
-        return res.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            return {
-                success: false,
-                message: error.response?.data?.message || "Unknown error occurred during fetching user data",
-            };
-        } else {
-            return {
-                success: false,
-                message: "Unexpected error during fetching user data",
-            };
-        }
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        success: false,
+        message:
+          error.response?.data?.message ||
+          "Unknown error occurred during fetching user data",
+      };
+    } else {
+      return {
+        success: false,
+        message: "Unexpected error during fetching user data",
+      };
     }
+  }
 }
