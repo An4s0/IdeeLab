8e2be9d9d72f1b1ef249ab912c653c728246f41c
Anas Almutary <00063q@gmail.com>
2025-05-23 21:04:36 +0300

    add middleware in web app

---
+++ apps/web/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import user from "@/lib/user";
+
+export async function middleware(request: NextRequest) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  const userData = await user.me(token);
+  console.log("userData", userData);
+
+  if (!userData.success) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (!userData.data?.user.verified) {
+    return NextResponse.redirect(new URL("/verify-email", request.url));
+  }
+}
+
+export const config = {
+  matcher: [],
+};
