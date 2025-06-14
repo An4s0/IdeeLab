dd7e862a0b5312b91965117843ae81e33da2247f
Anas Almutary <00063q@gmail.com>
2025-06-14 06:05:20 +0300

    create routes folder

---
+++ apps/api/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router: Router = Router();
+
+router.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to IdeeLab API",
+    });
+});
+
+export default router;
