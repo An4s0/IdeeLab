5b8cf2ed15756a6af74ca7521af305c70d57a8a7
Anas Almutary <00063q@gmail.com>
2025-06-14 06:03:18 +0300

    add api app file

---
+++ apps/api/app.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: `../../.env` });
+
+import express, { Application } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import os from "os";
+import { dependencies } from "./package.json";
+import pool from './database';
+pool
+  .connect()
+
+const app: Application = express();
+const start = Date.now();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.API_PORT, async () => {
+  const ip =
+    Object.values(os.networkInterfaces())
+      .flat()
+      .find((i) => i?.family === "IPv4" && !i.internal)?.address || "localhost";
+
+  console.log(
+    `\n   \x1b[95m▲ Express v${dependencies.express}\x1b[0m ${process.env.NODE_ENV === "development" ? "(Dev mode)" : "(Prod mode)"}`,
+  );
+  console.log(`   - Local:        http://localhost:${process.env.API_PORT}`);
+  console.log(`   - Network:      http://${ip}:${process.env.API_PORT}\n`);
+  console.log(` \x1b[92m✓\x1b[0m Starting...`);
+  console.log(` \x1b[92m✓\x1b[0m Ready in ${Date.now() - start}ms`);
+});
