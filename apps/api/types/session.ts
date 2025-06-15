b10b9737e23e7229849943972aa4d9e9b5f9cd23
Anas Almutary <00063q@gmail.com>
2025-06-15 04:13:54 +0300

    add session type

---
+++ apps/api/types/session.ts
@@ -0,0 +1,9 @@
+export interface Session {
+    id: string;
+    user_id: string;
+    token: string;
+    ip_address: string;
+    user_agent: string;
+    last_seen_at: string;
+    created_at: string;
+}
