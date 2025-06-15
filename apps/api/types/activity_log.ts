bd213c5fdafac424044e708f157a3a291c04ea4e
Anas Almutary <00063q@gmail.com>
2025-06-15 04:13:53 +0300

    add activity log type

---
+++ apps/api/types/activity_log.ts
@@ -0,0 +1,11 @@
+export interface ActivityLog {
+    id: string;
+    user_id: string;
+    ip_address: string;
+    user_agent: string;
+    action: string;
+    description: string;
+    target_type: string;
+    target_id: string;
+    created_at: string;
+}
