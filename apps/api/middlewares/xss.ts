0afd0ad461f5f846a89670c041643000f17dd117
Anas Almutary <00063q@gmail.com>
2025-06-15 13:32:26 +0300

    change value type to unknown

--- apps/api/middlewares/xss.ts
+++ apps/api/middlewares/xss.ts
@@ -1,17 +1,19 @@
-import { NextFunction, Request, Response } from 'express';
-import xss from 'xss';
+import { NextFunction, Request, Response } from "express";
+import xss from "xss";
 
-function sanitizeValue(value: any): any {
-  if (typeof value === 'string') {
+function sanitizeValue(value: unknown): unknown {
+  if (typeof value === "string") {
     return xss(value);
   } else if (Array.isArray(value)) {
     return value.map(sanitizeValue);
-  } else if (typeof value === 'object' && value !== null) {
+  } else if (typeof value === "object" && value !== null) {
     // If the value is an object, recursively sanitize its properties
-    const sanitizedObj: any = {};
-    for (const key in value) {
+    const sanitizedObj: Record<string, unknown> = {};
+    for (const key in value as Record<string, unknown>) {
       if (Object.prototype.hasOwnProperty.call(value, key)) {
-        sanitizedObj[key] = sanitizeValue(value[key]);
+        sanitizedObj[key] = sanitizeValue(
+          (value as Record<string, unknown>)[key],
+        );
       }
     }
     return sanitizedObj;
@@ -21,7 +23,7 @@ function sanitizeValue(value: any): any {
 }
 
 export const XSS = (req: Request, res: Response, next: NextFunction) => {
-  // Sanitize request body 
+  // Sanitize request body
   if (req.body) {
     for (const key in req.body) {
       if (Object.prototype.hasOwnProperty.call(req.body, key)) {
@@ -34,7 +36,15 @@ export const XSS = (req: Request, res: Response, next: NextFunction) => {
   if (req.query) {
     for (const key in req.query) {
       if (Object.prototype.hasOwnProperty.call(req.query, key)) {
-        req.query[key] = sanitizeValue(req.query[key]);
+        const sanitizedValue = sanitizeValue(req.query[key]);
+        //
+        if (
+          typeof sanitizedValue === "string" ||
+          Array.isArray(sanitizedValue) ||
+          sanitizedValue === undefined
+        ) {
+          req.query[key] = sanitizedValue as string | string[] | undefined;
+        }
       }
     }
   }
