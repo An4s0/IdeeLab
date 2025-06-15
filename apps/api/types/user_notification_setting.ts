af142801a0599aa95ccf15baf91e9ef428ecee05
Anas Almutary <00063q@gmail.com>
2025-06-15 04:13:54 +0300

    add user notification setting type

---
+++ apps/api/types/user_notification_setting.ts
@@ -0,0 +1,7 @@
+export interface UserNotificationSetting {
+    id: string;
+    user_id: string;
+    type: string; // e.g., 'idea_voted', 'idea_commented', 'idea_approved', etc.
+    is_enabled: boolean;
+    created_at: string;
+}
