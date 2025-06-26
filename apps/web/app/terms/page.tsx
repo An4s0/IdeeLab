ab81ab8088753160772988415d1615c6c63e37d7
Anas Almutary <00063q@gmail.com>
2025-06-26 14:39:13 +0300

    update terms page

--- apps/web/app/terms/page.tsx
+++ apps/web/app/terms/page.tsx
@@ -1,11 +1,11 @@
 import { Metadata } from "next";
+import TermsPage from "./terms";
 export const metadata: Metadata = {
   title: "Terms of Service",
   description:
     "Review the terms and conditions for using IdeeLab to explore, share, and manage your programming ideas.",
 };
 
-import TermsPage from "./terms";
 
 export default function terms() {
   return <TermsPage />;
