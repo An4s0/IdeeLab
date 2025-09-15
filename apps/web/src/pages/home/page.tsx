48cf6177a1823440c47cefa25301e477aeb5abec
Anas Almutary <00063q@gmail.com>
2025-09-15 15:46:04 +0300

    add explore button

--- apps/web/src/pages/home/page.tsx
+++ apps/web/src/pages/home/page.tsx
@@ -1,5 +1,6 @@
 import { useTitle } from "@/hooks/useTitle";
 import { Header } from "@/components/header";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   useTitle("IdeeLab | Where coding ideas come alive");
@@ -7,7 +8,14 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Link
+          to="/explore"
+          className="hidden md:block fixed bottom-5 right-5 px-4 py-2 bg-primary text-white rounded-full shadow-lg hover:bg-primary/90"
+        >
+          Explore
+        </Link>
+      </main>
     </>
   );
 }
