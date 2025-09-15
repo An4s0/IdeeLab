607bdcabbd56f9c7d84a58934c08acbdb1ec30df
Anas Almutary <00063q@gmail.com>
2025-09-15 17:55:00 +0300

    run format script

--- apps/web/src/pages/home/page.tsx
+++ apps/web/src/pages/home/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
         <Link
           to="/explore"
           className="hidden md:block fixed bottom-5 right-5 px-4 py-2 bg-primary text-white rounded-full shadow-lg hover:bg-primary/90"
-          style={{ textShadow: '1px 1px 2px rgba(0,0,0,0.5)' }}
+          style={{ textShadow: "1px 1px 2px rgba(0,0,0,0.5)" }}
         >
           Explore
         </Link>
