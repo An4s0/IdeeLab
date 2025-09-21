99f8725011454851bca62cab22ece36c91ed11f9
Anas Almutary <00063q@gmail.com>
2025-09-21 21:42:40 +0300

    add ideas page

--- apps/web/src/routes.tsx
+++ apps/web/src/routes.tsx
@@ -5,6 +5,7 @@ import { Loading } from "@/components/loading";
 export default function AppRoutes() {
   const NotFoundPage = React.lazy(() => import("./pages/404/page"));
   const HomePage = React.lazy(() => import("./pages/home/page"));
+  const IdeasPage = React.lazy(() => import("./pages/ideas/page"));
   const TermsPage = React.lazy(() => import("./pages/terms/page"));
   const PrivacyPage = React.lazy(() => import("./pages/privacy/page"));
   const AboutPage = React.lazy(() => import("./pages/about/page"));
@@ -14,6 +15,7 @@ export default function AppRoutes() {
     <Suspense fallback={<Loading />}>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/ideas" element={<IdeasPage />} />
         <Route path="/terms" element={<TermsPage />} />
         <Route path="/privacy" element={<PrivacyPage />} />
         <Route path="/about" element={<AboutPage />} />
