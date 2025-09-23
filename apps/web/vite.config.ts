d715e20669eb977377471cbc3b8cbafa28610bda
Anas Almutary <00063q@gmail.com>
2025-09-23 15:30:15 +0300

    uninstall rollup-plugin-visualizer package

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({
-      filename: "dist/bundle-report.html",
-      template: "treemap",
-      gzipSize: true,
-      brotliSize: true,
-    }),
   ],
   resolve: {
     alias: {
