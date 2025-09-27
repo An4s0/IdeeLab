0093e5c5cc45350039970178330f66ebc90833d7
Anas Almutary <00063q@gmail.com>
2025-09-27 20:01:55 +0300

    add main layout

---
+++ apps/web/src/components/layouts/main.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import { Header, BottomBar } from "../navigation";
+
+export function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main className="p-3 mt-16">{children}</main>
+      <BottomBar />
+    </>
+  );
+}
